0f4e8441fc5d09f634d535ea12c3410f172c3029
Abdullah Al Mahmud <abdullahalmahmud325@gmail.com>
2024-07-12 12:07:12 +0600

    Valid anagram, Group anagrams

---
+++ Algorithms/Group_anagrams.cpp
@@ -0,0 +1,182 @@
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+*/
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*
+## Approach 1 (Brute force) : [TLE]
+ Repeatedly Take two strings and check if they are anagram.
+
+## Code :
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        int index = -1;
+        vector<bool> flag(strs.size(),false);
+        for(int i = 0;i<strs.size();i++) {
+            if(flag[i]) continue;
+            res.push_back({});
+            index++;
+            res[index].push_back(strs[i]);
+            flag[i] = true;
+            for(int j= i+1 ;j<strs.size();j++) {
+                if(flag[j]) continue;
+                if(strs[i].size() != strs[j].size()) continue;
+                vector<int> a(26,0);
+                vector<int> b(26,0);
+                for(int k=0;k<strs[i].size();k++)
+                    a[strs[i][k]-'a']++;
+                for(int l=0;l<strs[j].size();l++)
+                    b[strs[j][l]-'a']++;
+                bool f = true;
+                for(int k=0;k<strs[i].size();k++)
+                        if(a[strs[i][k]-'a']!=b[strs[i][k]-'a'])
+                            {
+                                f = false;
+                                break;
+                            }
+                if(f) {
+                    res[index].push_back(strs[j]);
+                    flag[j] = true;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+## Time Complexity : O(n^3)
+*/
+
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+
+
+/*
+## Approach 2 (Using map) : [Accepted]
+ Take a vector<int> as the key to store character frequency.
+ All the strings with the same frequency will be mapped to the same key.
+
+## Code :
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<vector<int>,vector<string>> m;
+        for(string s : strs) {
+            vector<int> a(26,0);
+            for(auto ch : s)  {
+                a[ch - 'a']++;
+            }
+            if(m.find(a) == m.end()) {
+                m[a] = {};
+                m[a].push_back(s);
+            }
+            else {
+                m[a].push_back(s);
+            }
+        }
+        map<vector<int>,vector<string>> :: iterator it ;
+        it = m.begin();
+        vector<vector<string>> res;
+        while(it!= m.end()) {
+            res.push_back(it->second);
+            it++;
+        }
+        return res;
+
+    }
+};
+*/
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*
+## Approach 3 (using hash table) : [Accepted]
+Iterate through the vector of strings.
+Sort the string and push it to the hash table.
+
+## Code :
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> u_map;
+        for(auto s : strs) {
+            string t = s;
+            sort(t.begin(),t.end());
+            if(u_map.find(t) == u_map.end()) {
+                u_map[t] = {};
+                u_map[t].push_back(s);
+            }
+            else {
+                u_map[t].push_back(s);
+            }
+        }
+        vector<vector<string>> res;
+        auto it = u_map.begin();
+        while(it!=u_map.end()) {
+            res.push_back(it->second);
+            it++;
+        }
+        return res;
+    }
+};
+*/
+
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+/*
+## Approach 4 (Using hash map but more efficient) : [Accepted]
+
+ Take one string and sort it. Now instead of pushing it to the hash table, store it directly in the result vector.
+
+## Code :
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,int> u_map;
+        vector<vector<string>> res;
+        int index = 0;
+        for(auto s : strs) {
+            string t = s;
+            sort(t.begin(),t.end());
+            if(u_map.find(t) == u_map.end()) {
+                u_map[t] = index;
+                res.push_back({});
+                res[index].push_back(s);
+                index++;
+            }
+            else {
+                res[u_map[t]].push_back(s);
+            }
+        }
+        return res;
+    }
+};
+*/
---
+++ Algorithms/Valid_Anagram.cpp
@@ -0,0 +1,114 @@
+
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+*/
+
+// Constraints
+/*
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+## Approach 1 (Sorting) : [Accepted]
+
+ We can sort the two strings and then match all the characters one by one
+
+
+## Code :
+
+    class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            if(s.size() != t.size()) return false;
+            sort(s.begin(),s.end());
+            sort(t.begin(),t.end());
+            for(int i = 0 ;i<s.size();i++) {
+                if(s[i] != t[i]) return false;
+            }
+            return true;
+        }
+    };
+
+## Time complexity : O(nlogn)
+
+*/
+
+
+/******************************************************************************************/
+
+
+/*
+## Approach 2 (Count frequency) : [Accepted]
+ Count the frequency of the characters in both strings,
+ if the frequency count is same in both of the strings then return true else return false.
+
+
+ ## Code : 
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int> a(26,0);
+        vector<int> b(26,0);
+        for(int i=0;i<s.size();i++)
+            a[s[i]-'a']++;
+        for(int i=0;i<t.size();i++)
+            b[t[i]-'a']++;
+        for(int i=0;i<t.size();i++)
+                if(a[t[i]-'a']!=b[t[i]-'a'])
+                    return false;
+        return true;
+    }
+};
+
+## Time complexity : O(n)
+*/
+
+/*************************************************************************************/
+
+/*
+## Approach 3 (Using hash table) : [Accepted]
+ The idea is to store the count of each character in one string using a map where key is the character itself and value is the count of appearance of that character.
+ Then we traverse the second string and if we don't find a character in the map we return false immediately, and if we find a character in the map then we decrease the count of that character.
+ At last, if the map is empty then we return true, otherwise false.
+
+ ## Code : 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        else {
+            unordered_map<char,int> m;
+            for(int i=0;i<s.size();i++) {
+                if(m.empty()) m[s[i]] = 1;
+                else if(m.find(s[i]) == m.end()) m[s[i]] = 1;
+                else m[s[i]]++;
+            }
+            for(int i=0;i<t.size();i++) {
+                if(m.find(t[i]) == m.end()) return false;
+                else {
+                    m[t[i]]--;
+                    if(m[t[i]] == 0) m.erase(t[i]);
+                }
+            }
+            return m.empty();
+        }
+        return true;
+    }
+};
+
+## Time complexity : O(n)
+*/
+
+
+
+
